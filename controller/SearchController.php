b906f742a2747982096afd6d18791d19c59b73c1
DimaJobs <doroshuk33@yandex.ru>
2019-10-23 17:02:37 +0300

    SearchCoding2

--- controller/SearchController.php
+++ controller/SearchController.php
@@ -10,22 +10,30 @@ class SearchController extends DisplayController
     protected $messageEmpty;
 
 
+
     public function execute ($request, $response, $args) {
+        //Защита от повторной отправки данных формы при обновлении страницы (при получении данных делаем редирект на страницу поиска)
             //Получение POST запроса при поиске точки
-            if ($request->isPost()) { 
-                    //Получение строки введенной в поиск пользователем
-                    $posts_data = $request->getParsedBody();         
-                        if($result = $this->searchDatainBD($posts_data)){
-                            $this->dataRequest = $result;                       
-                        }; 
-            }  
+            if ($request->isPost()) {
+                //Получение строки введенной в поиск пользователем
+                    $posts_data = $request->getParsedBody();
+                    $_SESSION['requestDataSearch'] = $posts_data['searchRequest'];
+                return $response->withRedirect($uri = $request->getUri()->getpath());
+            }
+
+            if ($this->session['requestDataSearch']) {
+                if ($result = $this->searchDatainBD($this->session['requestDataSearch'])) {
+                    $this->dataRequest = $result;
+                }
+            }
         return $this->display($request, $response, $args);
     }
     protected function display($request, $response, $args) {
-        
-        
         //Формирование разрешения для отображения блоков в зависимости от роли
         $this->getBlockShowRole();
+        //Подключение необходимых скриптов
+        $this->page_script = $this->getScripts();
+
         $this->title .= "Search";
         $this->mainbar = $this->mainBar();
         parent::display($request, $response, $args);
@@ -46,28 +54,44 @@ class SearchController extends DisplayController
                         'message'                   => $this->messageEmpty
                 ]);
     }
+
+    //Получить необходимые скрипы для отображения страницы
+    protected function getScripts () {
+        return [
+            '/js/point-del.js'
+        ];
+    }
     
     protected function searchDatainBD ($data) {
-        $request = $data['searchRequest'];
-        $request = $this->clear_str($request);
+        $request = $this->clear_str($data);
         $array = array ();
         //Получаем все точки в бд
         $DataReturn = $this->model->getDataPoint();
         
         //Проверяем совпадения строки запроса с полем ip 
         foreach ($DataReturn as $item) {
-            if (strpos($item['ip'], $request) !== FALSE) {
+            if (stripos($item['ip'], $request) !== FALSE) {
                 $array[] = $item;
             }
         }
-       
+
         //Проверяем полученные данные
         if ($array) {
             return $array;
         }else {
-            $this->messageEmpty = "Нет совпадений";
-            return FALSE;
+            //Проверяем совпадения строки запроса с полем name
+            foreach ($DataReturn as $item) {
+                if (mb_stripos ($item['name'], $request) !== FALSE) {
+                   // echo "GOOD"; exit;
+                    $array[] = $item;
+                }
+            }
+
         }
+        if ($array) {
+            return $array;
+        }
+
         
     }
 }
